42dc58261187eb7eb3ac06f0c627328618c5a78a
Gerik Castillo <geriklc@gmail.com>
2023-05-05 02:10:46 -0400

    created header component and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import PaymentInfo from "./Components/PaymentInfo";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./Components/Header/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <Header />
         <PaymentInfo />
-      </header>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,40 @@
+import "./Header.scss";
+import amex from "../../assets/AMEXheader.png";
+import card from "../../assets/amex-card-removebg-preview.png"
+
+function Header() {
+  return (
+    <header className="header">
+      <section className="header__container">
+        <p className="header__icon-text">
+          <span className="material-symbols-outlined header__icon-menu">
+            menu
+          </span>
+          Menu
+        </p>
+        <img src={amex} alt="amex logo" className="header__logo"></img>
+        <div className="header__wrapper">
+        <span class="material-symbols-outlined header__icon-search">
+          search
+        </span>
+          <a className="header__link">Contact Us</a>
+          <a className="header__link">Log Out</a>
+        </div>
+      </section>
+      <nav className="nav">
+        <div className="nav__wrapper">
+        <a className="nav__link nav__link--current">Home</a>
+        <a className="nav__link">Statement</a>
+        <a className="nav__link">Payments</a>
+        <a className="nav__link">Manage Accounts</a>
+        <a className="nav__link">Information</a>
+        </div>
+        <div className="nav__container">
+        <img src={card} className="nav__img"></img>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
