a8c94ea1f51dff9ff59f435e65d744dbaaec0c5c
Alex Zaloga <alexandrazaloga@gmail.com>
2023-05-05 00:20:07 -0400

    added partials and variables

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Industry Project</p>
+        <span class="material-symbols-outlined">home</span>
       </header>
     </div>
   );
