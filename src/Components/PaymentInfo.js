f2560cb56e4793e7e1cb11ca983eb4282a2d712b
Alex Zaloga <alexandrazaloga@gmail.com>
2023-05-05 01:49:06 -0400

    completed paymentinfo component

--- src/Components/PaymentInfo.js
+++ src/Components/PaymentInfo.js
@@ -2,8 +2,22 @@ import "./PaymentInfo.scss";
 
 function PaymentInfo() {
   return (
-    <div className="PaymentInfo">
-      <p>PaymentInfo</p>
+    <div className="Payment-Info">
+      <section className="Payment-Info__box">
+        <div className="Payment-Info__textbox">
+          <h2 className="Payment-Info__bold">Payments & Credits</h2>
+          <h2 className="Payment-Info__bold">Previous Charges & Adjustments</h2>
+          <h2 className="Payment-Info__bold">New Charges & Adjustments</h2>
+          <h2 className="Payment-Info__regular">Check Spending Power</h2>
+        </div>
+      </section>
+      <section className="Payment-Info__boxtwo">
+        <h2 className="Payment-Info__amount">Amount due is on</h2>
+        <h2 className="Payment-Info__large">15 May</h2>
+        <div className="Payment-Info__button-container">
+          <button className="Payment-Info__button">Make Payment</button>
+        </div>
+      </section>
     </div>
   );
 }
